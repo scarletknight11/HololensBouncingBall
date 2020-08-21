16d1fb470641a800465e4b97bb05dc8d553adf3e
Sanjit Singh <sanjitpsingh@yahoo.com>
2020-08-21 02:33:13 -0400

    Worked on Design & Prototpying for Microsoft Hololens 2 using MRTK

---
+++ Assets/Scripts/SpawnBallThing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBallThing : MonoBehaviour {
+
+    public GameObject ballPrefab;
+
+    public void SpawnBall()
+    {
+        Vector3 position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        GameObject spawnedBall = Instantiate(ballPrefab, position, transform.rotation);
+    }
+}
